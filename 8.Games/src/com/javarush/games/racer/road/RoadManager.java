cf9fefbf16b8b125ed28cfbc6407529be22f6500
Phoenix Kirill <phoenix-wrc@mail.ru>
2020-05-25 02:48:40 +1000

    Решил 11  задачу в Racer

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -6,4 +6,9 @@ public class RoadManager {
 	public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
 	public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
 
+	private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
+		if(type != RoadObjectType.THORN) return null;
+		if(type == RoadObjectType.THORN) return new Thorn(x,y);
+		return null;
+	}
 }
