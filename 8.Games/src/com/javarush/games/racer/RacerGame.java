5c656562f3dc62a9e99318ffb4e0ddf40e0d6f53
Phoenix Kirill <phoenix-wrc@mail.ru>
2020-05-24 21:51:54 +1000

    Решил вторую задачу в Racer

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -3,11 +3,25 @@ package com.javarush.games.racer;
 import com.javarush.engine.cell.*;
 
 public class RacerGame extends Game {
-	public static final int WIDTH = 64, HEIGHT = 64;
+	public static final int WIDTH = 64, HEIGHT = 64, CENTER_X = WIDTH/2, ROADSIDE_WIDTH = 14;
+
+	private void createGame() {
+		drawScene();
+	}
+
+	private void drawScene() {
+		drawField();
+	}
+
+	private void drawField() {
+
+	}
 
 	@Override
 	public void initialize() {
 		showGrid(false);
 		setScreenSize(WIDTH,HEIGHT);
+
+		createGame();
 	}
 }
