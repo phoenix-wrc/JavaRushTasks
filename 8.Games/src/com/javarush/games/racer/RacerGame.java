2c77a9a7742c243433ae8b9bb57a99197c578f04
Phoenix Kirill <phoenix-wrc@mail.ru>
2020-05-24 21:43:31 +1000

    Решил первую задачу в Racer

---
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -0,0 +1,13 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class RacerGame extends Game {
+	public static final int WIDTH = 64, HEIGHT = 64;
+
+	@Override
+	public void initialize() {
+		showGrid(false);
+		setScreenSize(WIDTH,HEIGHT);
+	}
+}
