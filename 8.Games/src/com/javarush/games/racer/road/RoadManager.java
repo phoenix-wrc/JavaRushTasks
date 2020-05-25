66b3a7f48263803be250ee1e6ffc879b3ea14568
Phoenix Kirill <phoenix-wrc@mail.ru>
2020-05-25 20:48:23 +1000

    Решил задачу 20 в Racer

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -13,8 +13,17 @@ public class RoadManager {
 	public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
 	private static final int FIRST_LANE_POSITION = 16;
 	private static final int FOURTH_LANE_POSITION = 44;
+	private static final int PLAYER_CAR_DISTANCE = 12;
 	private List<RoadObject> items = new ArrayList<>();
 
+	private boolean isRoadSpaceFree(RoadObject object) {
+		for (RoadObject item: items) {
+			if(item.isCollisionWithDistance(object, PLAYER_CAR_DISTANCE)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	private void generateRegularCar(Game game) {
 		int carTypeNumber = game.getRandomNumber(4);
 		if(game.getRandomNumber(100) < 30) {
@@ -75,7 +84,7 @@ public class RoadManager {
 		int x = game.getRandomNumber(FIRST_LANE_POSITION,FOURTH_LANE_POSITION);
 		int y = -1 * RoadObject.getHeight(type);
 		RoadObject ro = createRoadObject(type, x, y);
-		if(ro != null) items.add(ro);
+		if(ro != null && isRoadSpaceFree(ro)) items.add(ro);
 	}
 
 	private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
